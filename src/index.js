1c531c5269737e7ae685657bed69dfa6c37eb484
Samrendra <samrendrakumar893@gmail.com>
2023-10-30 12:45:20 +0530

    Added roles and user_roles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express=require('express');
 const bodyParser = require('body-parser');
 const {PORT}= require('./config/serverConfig')
 const apiRoutes = require('./routes/index')
+const db= require('./models/index')
+const {User,Role} = require('./models/index');
 const UserService= require('./services/user-services')
 const app = express() ;
 const prepareAndStartServer = () => {
@@ -10,6 +12,15 @@ const prepareAndStartServer = () => {
     app.use('/api',apiRoutes)
     app.listen(PORT,async()=>{
         console.log(`Server started at Port : ${PORT}`);
+        if(process.env.DB_SYNC)
+        {
+            db.sequelize.sync({alter:true});
+        }
+        const u1=await User.findByPk(3);
+        const r1=await Role.findByPk(2) ;
+       const response=await u1.getRoles() ;
+       console.log(response);
+       
         //const repo=new UserRepository() ;
         //const response = await repo.getById(1) ;
         //console.log(response) ;
