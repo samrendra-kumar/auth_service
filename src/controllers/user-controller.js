1f210de52c585882c0a41fb76e13e63684c2360e
Samrendra <samrendrakumar893@gmail.com>
2023-10-30 09:15:55 +0530

    Authentication added

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -32,7 +32,7 @@ try
 {
  const response=await userService.signIn(req.body.email,req.body.password);
  return res.status(200).json({
-  data:{},
+  data:response,
   success:true ,
   err:{} ,
   message:'successfully signed in'
@@ -49,6 +49,29 @@ try
         })
 }
 }
+
+const isAuthenticated = async(req,res)=>{
+  try{
+    const token= req.headers['x-access-token']
+    const response=await userService.isAuthenticated(token);
+    return res.status(200).json({
+      success:true ,
+      err:{} ,
+      data:response ,
+      message:'user is authenticated and token is valid '
+    })
+  }catch(error)
+  {
+    console.log(error) ;
+    return res.status(500).json({
+        message:'Something went wrong',
+        data:{} ,
+        success:false ,
+        err:error 
+    })
+}
+  }
+
 module.exports = {
-    create,signIn
+    create,signIn,isAuthenticated
 }
